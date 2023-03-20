85124c9c8e823b24772b44338bf94adcf2ab8981
jpablop123 <juanpablopinto123@icloud.com>
2023-03-20 20:42:31 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").classList.add(generatesuit());
+  document.querySelector(".card").innerHTML = generatenumber();
+};
+let generatenumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let indexnumber = Math.floor(Math.random() * numbers.length);
+
+  return numbers[indexnumber];
+};
+let generatesuit = () => {
+  let suit = ["diamond", "spade", "heart", "club"];
+
+  let indexsuit = Math.floor(Math.random() * suit.length);
+  return suit[indexsuit];
 };
